efc1603f7ec4a09d3e0d36c210e5734aa2cfda35
Melvin Paulsam <003melvin@gmail.com>
2023-05-14 00:00:46 +0530

    Added the landmark detection

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,81 @@
 import { useState } from 'react'
 import {useRef} from 'react'
-
+import { drawMesh } from "./utilities.js";
 import reactLogo from './assets/react.svg'
 import './App.css'
 import * as tf from "@tensorflow/tfjs";
 import * as facemesh from "@tensorflow-models/facemesh" 
+import Webcam from "react-webcam";
 function App() {
-  const [count, setCount] = useState(0)
+  const webcamRef=useRef(null);
+  const canvasRef=useRef(null);
+  
+  const runFacemesh=async()=>{
+    const net=await facemesh.load(
+      {input:video}
+    );
+    setInterval(() => {
+      detect(net);
+    }, 10);
+  }
+  const detect=async (net)=>{
+    if(
+      typeof webcamRef.current!=="undefined" &&
+      webcamRef.current!==null &&
+      webcamRef.current.video.readyState===4
+    ){
+      const video=webcamRef.current.video;
+      const videoWidth=webcamRef.current.video.videoWidth;
+      const videoHeight=webcamRef.current.video.videoHeight;
 
+      webcamRef.current.video.width=videoWidth;
+      webcamRef.current.video.height=videoHeight;
+
+      canvasRef.current.width=videoWidth;
+      canvasRef.current.height=videoHeight;
+
+      const face = await net.estimateFaces({input:video});
+      console.log(face);
+
+ 
+      const ctx = canvasRef.current.getContext("2d");
+      requestAnimationFrame(()=>{drawMesh(face, ctx)});
+    }
+    
+  };
+  useEffect(()=>{runFacemesh()}, []);
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+      <Webcam 
+      ref={webcamRef}
+      style={{
+        position:"relative",
+        marginLeft:"auto",
+        marginRight:"auto",
+        left:0,
+        right:0,
+        textAlign:'center',
+        zIndex:9,
+        width:1000,
+        height:480,
+      }}/>
+      <canvas 
+      ref={canvasRef}
+      style={{ position:"relative",
+      marginLeft:"auto",
+      marginRight:"auto",
+      left:0,
+      right:0,
+      textAlign:'center',
+      zIndex:9,
+      width:1000,
+      height:480.
+        
+      }}/>
+    </header>
     </div>
-  )
-}
+  );
+    }
 
 export default App
